09d89b496a0d34b0114abe5ca8e69406311ef45d
flavionevesfsn <flavioneves89@gmail.com>
2024-09-18 00:36:28 -0300

    first commit

---
+++ src/main/java/com/infnet/actions_teste/ActionsTesteApplication.java
@@ -0,0 +1,13 @@
+package com.infnet.actions_teste;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActionsTesteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActionsTesteApplication.class, args);
+	}
+
+}
